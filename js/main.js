e0df904506604c79f4ecd9f397e656f8ae52aee6
abdulhayev-nuriddin <abdulxayevnuriddin01@gmail.com>
2025-02-13 21:05:45 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -17,3 +17,23 @@ function closeModal(event) {
         document.getElementById("modal").style.display = "none";
     }
 }
+
+// ------------------------
+const slides = document.querySelector('.slides');
+const slide = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+let index = 0;
+
+const showSlide = (i) => {
+  index = (i + slide.length) % slide.length;
+  slides.style.transform = `translateX(-${index * 100}%)`;
+};
+
+const nextSlide = () => showSlide(index + 1);
+const prevSlide = () => showSlide(index - 1);
+
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
+
+setInterval(nextSlide, 3000); 
